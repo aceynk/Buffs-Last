a51784944d23bb82e74cc64db8ce549c9dea7e88
Aloe <162494218+aceynk@users.noreply.github.com>
2024-08-11 19:25:24 -0700

    added config option to exclude endless buffs

---
+++ Config.cs
@@ -0,0 +1,6 @@
+namespace Buffs_Last;
+
+public sealed class ModConfig
+{
+    public bool ExcludeEndless { get; set; } = true;
+}
--- ModEntry.cs
+++ ModEntry.cs
@@ -1,4 +1,5 @@
-﻿using StardewModdingAPI;
+﻿using GenericModConfigMenu;
+using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.GameData.Buffs;
@@ -13,11 +14,15 @@ public class ModEntry : Mod
 	}
 
 	public static IMonitor _log = null!;
+	public static ModConfig Config;
 
 	public override void Entry(IModHelper helper)
 	{
 		_log = Monitor;
 		Helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+		Helper.Events.GameLoop.GameLaunched += InitConfig;
+
+		Config = Helper.ReadConfig<ModConfig>();
 	}
 
 	private void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
@@ -29,12 +34,42 @@ public class ModEntry : Mod
 		
 		foreach (string key in buffs.Select(v => v.Key))
 		{
-			bool IsIncludedBuff = !System.Text.RegularExpressions.Regex.IsMatch(key, ExcludedBuffs);
+			bool IsIncludedBuff = !Config.ExcludeEndless
+			                      || !System.Text.RegularExpressions.Regex.IsMatch(key, ExcludedBuffs);
 			persistentBuffs[key] = IsIncludedBuff;
 		}
 		
 		persistentBuffs["food"] = true;
 		
 		Log("Successfully processed all Buffs.");
+	}
+	
+	private void InitConfig(object? sender, GameLaunchedEventArgs e)
+	{
+		var menu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+
+		if (menu is null)
+		{
+			return;
+		}
+        
+		menu.Register(
+			mod: ModManifest,
+			reset: () => Config = new ModConfig(),
+			save: () => Helper.WriteConfig(Config)
+		);
+        
+		menu.AddSectionTitle(
+			mod: ModManifest,
+			text: () => Helper.Translation.Get("GMCM.MainTitle")	
+		);
+        
+		menu.AddBoolOption(
+			mod: ModManifest,
+			name: () => Helper.Translation.Get("GMCM.ExcludeEndless.Name"),
+			tooltip: () => Helper.Translation.Get("GMCM.ExcludeEndless.Desc"),
+			getValue: () => Config.ExcludeEndless,
+			setValue: v => Config.ExcludeEndless = v
+		);
 	}
 }
